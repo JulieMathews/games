7b02a6cbd1a690cf8d49ce87fade2860a3642821
Julie Mathews <juliem.forever@gmail.com>
2019-01-09 21:34:39 -0800

    update

---
+++ unit-4-game/assets/javascript/game.js
@@ -0,0 +1,11 @@
+// click blue, get random number
+// click green, get random number
+// click purple, get random number
+// click yellow, get random number
+// pink num is the random goal number by adding blu, grn, pur, ylw
+// blue num is adding each pressed blu, grn, pur, ylw
+// won by equaling goal
+// lost going over goal
+// restart when lost
+// restart when won
+
