00d460d3f1302d540690512bf870562d95b5f202
Julie Mathews <juliem.forever@gmail.com>
2019-01-12 12:45:13 -0800

    update finish

--- unit-4-game/assets/javascript/game.js
+++ unit-4-game/assets/javascript/game.js
@@ -3,20 +3,23 @@ var wins= 0;
 var losses = 0;
 
 // click blue, get random number
-var num1 = Math.floor(Math.random()*10)+3
+var num1 = Math.floor(Math.random()*10)+1
 
 // click green, get random number
-var num2 = Math.floor(Math.random()*10)+3
+var num2 = Math.floor(Math.random()*10)+1
 
 // click purple, get random number
-var num3 = Math.floor(Math.random()*10)+3
+var num3 = Math.floor(Math.random()*10)+1
 
 // click yellow, get random number
-var num4 = Math.floor(Math.random()*10)+3
+var num4 = Math.floor(Math.random()*10)+1
+
+// random blu box goal num
+var Random = Math.floor(Math.random()*80)+20
+
 
 // blue num is the random goal number 
 $(document).ready(function(){
-    var Random = Math.floor(Math.random()*80)+20
     $('#randomNumber').text(Random);
     console.log(Random)
 
@@ -39,7 +42,7 @@ $('#one').on('click', function(){
         if (userTotal == Random){
           yay();
         }
-        else if ( serTotal > Random){
+        else if (userTotal > Random){
           loser();
         } 
   })  
@@ -75,20 +78,20 @@ $('#numberLosses').text(losses);
 
 // restart when lost/won
 function reset(){
-    Random=Math.floor(Math.random()*80)+20;
-    console.log(Random)
+    Random = Math.floor(Math.random()*80)+20;
     $('#randomNumber').text(Random);
-    num1 = Math.floor(Math.random()*10)+3;
-    num2 = Math.floor(Math.random()*10)+3;
-    num3 = Math.floor(Math.random()*10)+3;
-    num4 = Math.floor(Math.random()*10)+3;
+    console.log(Random)
+
+    num1 = Math.floor(Math.random()*10)+1;
+    num2 = Math.floor(Math.random()*10)+1;
+    num3 = Math.floor(Math.random()*10)+1;
+    num4 = Math.floor(Math.random()*10)+1;
     userTotal = 0;
     $('#finalTotal').text(userTotal);
     } 
 
 // tally won
 function yay(){
-    alert("You won!");
       wins++; 
       $('#numberWins').text(wins);
       reset();
@@ -96,7 +99,6 @@ function yay(){
 
 // tally lost
 function loser(){
-    alert ("You lose!");
       losses++;
       $('#numberLosses').text(losses);
       reset()
